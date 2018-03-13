7b6ba1aab8167d321da28f91270cf79ca7255512
answer_d <bboy.answer.d@gmail.com>
2018-03-13 17:12:03 +0900

    bug fix for IndexViewTest

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -3,13 +3,14 @@ import datetime
 from django.test import TestCase
 from django.utils import timezone
 from django.urls import reverse
+#from django.db import models
 
 from .models import Question
 
 # Create your tests here.
 def create_question(question_text, days):
     time = timezone.now() + datetime.timedelta(days=days)
-    return Question.objects.create(question_text, pub_date=time)
+    return Question.objects.create(question_text=question_text, pub_date=time)
 
 class QuestionModelTests(TestCase):
     def test_was_published_recently_with_future_question(self):
@@ -31,7 +32,7 @@ class QuestionIndexViewTests(TestCase):
     def test_no_questions(self):
         response = self.client.get(reverse('polls:index'))
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "No Polls are available")
+        self.assertContains(response, "No polls are available")
         self.assertQuerysetEqual(response.context['latest_question_list'], [])
 
     def test_past_question(self):
